598365edf8810e618aa227748705cd78c1e3741d
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-03 12:44:39 -0700

    update code

--- register.php
+++ register.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <section class="form-container">
+        <form method="post">
+           <h1>Register now</h1>
+           <input type="text" name="name" placeholder="enter your name" required>
+           <input type="emai" name="email" placeholder="enter you email" required>
+           <input type="password" name="password" placeholder="enter your password"required>
+           <input type="cpassword" name="number"placeholder="confirm password" required>
+           <input type="submit" value="register now" name="submit-btn" class="btn">
+           <p>already hava an account ?<a href="login.php">login now</a></p>
+        </form>
+ 
+    </section>
     
 </body>
 </html>
