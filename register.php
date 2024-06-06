162a459f48531ce852f17fcd0197625051cfd29c
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-06 00:22:27 -0700

    update

--- register.php
+++ register.php
@@ -5,14 +5,14 @@
  $filter_name = filter_var($_POST['name'], /*FILTER_SANITIZE_STRING*/);
    $name = mysqli_real_escape_string($conn, $filter_name);
 
-$filter_email = filter_var($_POST['email'], /*FILTER_SANITIZE_STRING*/ );
+$filter_email = filter_var($_POST['email'],  /*FILTER_SANITIZE_STRING*/ );
    $email = mysqli_real_escape_string($conn, $filter_email);
 
 $filter_password = filter_var($_POST['password'], /*FILTER_SANITIZE_STRING*/ );
    $password = mysqli_real_escape_string($conn, $filter_password);
 
 $filter_cpassword = filter_var($_POST['cpassword'], /*FILTER_SANITIZE_STRING*/ );
-   $cpassword = mysqli_real_escape_string($conn, $filter_cpassword);
+    $cpassword = mysqli_real_escape_string($conn, $filter_cpassword);
 
    $select_user = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
 
@@ -42,7 +42,7 @@ $filter_cpassword = filter_var($_POST['cpassword'], /*FILTER_SANITIZE_STRING*/ )
     <section class="form-container">
     <?php
     if(isset($message)){
-        foreach ($message as $message){
+        foreach ($message as $msg){
             echo '
             <div class="message">
             <span>'.$message.'</span>
