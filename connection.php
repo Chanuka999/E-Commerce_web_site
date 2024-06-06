ff615e33fc1b2b38eb6eb63404c9c88ff453f4b1
Chanuka999 <155728444+Chanuka999@users.noreply.github.com>
2024-06-05 23:31:54 -0700

    connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,5 @@
 <?php  
- $conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+ $conn = mysqli_connect("localhost","root","","shop_db","3307") or die('connection failed');
+
+?>
 
